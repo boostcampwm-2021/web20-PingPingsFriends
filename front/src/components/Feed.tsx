c60292220cc0bcd48624610d44a01702f3f696b2
kanghohyeong <hobro.kang@gmail.com>
2021-11-03 22:06:54 +0900

    refact: [#16] css 다루는 방식 변경
    하나의 styled-component 내에서 여러 클래스를 다루는 방식에서
    여러 styled-component로 각각 스타일을 다루는 방식으로 변경

--- front/src/components/Feed.tsx
+++ front/src/components/Feed.tsx
@@ -12,76 +12,82 @@ export type t_FeedJson = {
   text: string;
 };
 
-const FeedContainer = styled.div<{ imageURL: string }>`
+const FeedContainer = styled.div`
   background-color: white;
   width: 100%;
   height: 650px;
   flex-shrink: 0;
   border-radius: 30px;
   position: relative;
-  .header {
-    height: 50px;
-    display: flex;
-    align-items: center;
-    padding-left: 10px;
-    gap: 2px;
+`;
 
-    .vert_btn {
-      position: absolute;
-      right: 10px;
-    }
+const FeedHeader = styled.div`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+  gap: 2px;
+
+  .vert_btn {
+    position: absolute;
+    right: 10px;
   }
-  .Avatar {
-    width: 30px;
-    height: 30px;
-    border-radius: 100%;
-    background-color: black;
-  }
-  .contents_container {
-    width: 100%;
-    position: relative;
-    background-color: aliceblue;
-  }
-  .contents_container::after {
+`;
+
+const Avatar = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  background-color: black;
+`;
+
+const FeedContents = styled.div<{ imageURL: string }>`
+  width: 100%;
+  position: relative;
+  background-color: aliceblue;
+
+  &::after {
     padding-bottom: 100%;
     content: '';
-    background-image: ${(props) => 'url(' + props.imageURL + ')'};
+    background-image: ${(props) => `url(${props.imageURL})`};
     background-position: center;
     background-size: cover;
     display: block;
   }
-  .info_container {
-    display: flex;
-    position: relative;
-    gap: 5px;
-    padding: 3px 10px;
-    .time {
-      position: absolute;
-      right: 10px;
-      color: #c4c4c4;
-    }
+`;
+
+const FeedInfo = styled.div`
+  display: flex;
+  position: relative;
+  gap: 5px;
+  padding: 3px 10px;
+  .time {
+    position: absolute;
+    right: 10px;
+    color: #c4c4c4;
   }
-  .text_container {
-    padding: 0 10px;
-  }
+`;
+
+const FeedText = styled.div`
+  padding: 0 10px;
 `;
 
 const Feed = ({ json }: { json: t_FeedJson }) => {
   return (
-    <FeedContainer imageURL={json.imageURL}>
-      <div className="header">
-        <div className="Avatar"></div>
+    <FeedContainer>
+      <FeedHeader>
+        <Avatar />
         <span>{json.nickname}</span>
         <VertBtnSvg className="vert_btn" />
-      </div>
-      <div className="contents_container"></div>
-      <div className="info_container">
+      </FeedHeader>
+      <FeedContents imageURL={json.imageURL} />
+      <FeedInfo>
         <HeartBtnSvg />
         <CommentBtnSvg />
         <span>13</span>
         <span className="time">2시간 전</span>
-      </div>
-      <div className="text_container">{json.text}</div>
+      </FeedInfo>
+      <FeedText>{json.text}</FeedText>
     </FeedContainer>
   );
 };
