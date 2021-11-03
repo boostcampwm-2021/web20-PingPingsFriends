27ac6fc7d4a472e00a94006503c0b4fb0ee7ff6a
kanghohyeong <hobro.kang@gmail.com>
2021-11-03 17:20:30 +0900

    feat: [#12] 페이지 라우팅 초기화
    index에 BrowserRoute 추가
    app에 switch route 적용
    페이지 컴포넌트 생성

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import UserPage from './pages/UserPage';
+import RegisterPage from './pages/RegisterPage';
+import ExplorePage from './pages/ExplorePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route path="/register">
+          <RegisterPage />
+        </Route>
+        <Route path="/explore">
+          <ExplorePage />
+        </Route>
+        <Route path="/user">
+          <UserPage />
+        </Route>
+        <Route path="/">
+          <MainPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ front/src/pages/RegisterPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RegisterPage: React.FC = () => {
+  return <div>register page</div>;
+};
+
+export default RegisterPage;
