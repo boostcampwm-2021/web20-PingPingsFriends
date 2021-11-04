0d4844174a1b55ac3211180a6a0cbd78e640a5de
ludacirs <ludacirs@naver.com>
2021-11-04 15:11:35 +0900

    fix: [#20] Avatar 컴포넌트 테스트 코드 수정
    이미지 URL 변경

--- front/src/components/_common/Avatar/index.test.tsx
+++ front/src/components/_common/Avatar/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Avatar, { DEFAULT_IMG_SRC } from './Avatar';
+import Avatar from './Avatar';
 
 describe('Avatar 컴포넌트 테스트', function () {
   it('Avatar 이미지 url을 받아서 화면에 출력한다', function () {
@@ -12,6 +12,6 @@ describe('Avatar 컴포넌트 테스트', function () {
   it('Avatar 이미지 url을 받지 않으면 기본 url을 출력한다', function () {
     render(<Avatar />);
     const urlText = screen.getByRole(`img`) as HTMLImageElement;
-    expect(urlText.src).toBe(DEFAULT_IMG_SRC);
+    expect(urlText.src).toBe('http://localhost/default_avatar.png');
   });
 });
