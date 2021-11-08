ad537becf9b9103d9280794bcc0458535a156702
kanghohyeong <hobro.kang@gmail.com>
2021-11-09 01:51:05 +0900

    feat: [#50] 글로벌 css 속성 추가
    버튼 커서 설정을 위한 글로벌 css 생성

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './reset.css';
+import './global.css';
 
 import App from './App';
 ReactDOM.render(
