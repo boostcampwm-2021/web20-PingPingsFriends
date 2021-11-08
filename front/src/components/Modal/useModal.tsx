45d4ba7d2885f5a367f9bcac3ceb4eb2f3f72930
ludacirs <ludacirs@naver.com>
2021-11-09 02:32:03 +0900

    feat: [#75] Modal 컴포넌트 이벤트 로직 추가
    index.html에 react.createPortal을 위한 div 추가
    클릭시 모달 띄우기, 배경 클릭시 끄기, ESC 눌렀을 경우 끄기 기능 추가
    useModal 훅으로 분리 로직 분리

---
+++ front/src/components/Modal/useModal.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+export type ModalEvent = React.MouseEvent<HTMLDivElement> | React.KeyboardEvent<HTMLDivElement>;
+
+const useModal = ($elem = '#modal') => {
+  const [isShowing, setIsShowing] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const $portal = document.querySelector($elem) as Element;
+
+  const toggle = (event: ModalEvent): void => {
+    if ('key' in event && event.key === 'Escape') {
+      setIsShowing(!isShowing);
+      return;
+    }
+    const target = event.target as Element;
+    if (target.closest('.contents')) {
+      return;
+    }
+    setIsShowing(!isShowing);
+  };
+
+  useEffect(() => {
+    contentRef.current?.focus();
+  });
+
+  return {
+    isShowing,
+    toggle,
+    $portal,
+    contentRef,
+  };
+};
+
+export default useModal;
