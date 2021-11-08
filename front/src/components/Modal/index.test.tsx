b17f85a4b6fc7538a09dc842357109c71a4ae4b8
ludacirs <ludacirs@naver.com>
2021-11-09 00:12:11 +0900

    feat: [#75] Modal 컴포넌트 이벤트 테스트코드 작성
    서식지 클릭시 모달창 띄우기
    모달창 뒷 배경 클릭시 모달창 닫기
    ESC 입력시 모달창 닫기
    테스트 케이스 작성

--- front/src/components/Modal/index.test.tsx
+++ front/src/components/Modal/index.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Modal from './Modal';
+import userEvent from '@testing-library/user-event';
 
 describe('Modal 컴포넌트 테스트', function () {
   it('Modal 컴포넌트 생성 ', function () {
@@ -19,5 +20,33 @@ describe('Modal 컴포넌트 테스트', function () {
     const content = screen.getByText('test');
 
     expect(content).toBeInTheDocument();
+  });
+
+  it('모달창의 검은 부분을 클릭하면 모달창이 닫힌다', async function () {
+    const { container } = render(
+      <div>
+        <Modal>contents</Modal>
+      </div>
+    );
+
+    userEvent.click(container);
+
+    expect(screen.queryByText('contents')).not.toBeInTheDocument();
+  });
+
+  it('컨텐츠 창에서 ESC를 누르면 모달창이 닫힌다', async function () {
+    render(
+      <div>
+        <Modal>
+          <div tabIndex={0}>contents</div>
+        </Modal>
+      </div>
+    );
+
+    const contents = screen.getByText('contents');
+
+    userEvent.type(contents, '{esc}');
+
+    expect(screen.queryByText('contents')).not.toBeInTheDocument();
   });
 });
