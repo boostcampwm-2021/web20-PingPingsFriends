a1e6147e5be9ea7aa5522583fde4a2e2f62af4bd
kanghohyeong <hobro.kang@gmail.com>
2021-11-09 16:36:02 +0900

    fix: FAB 반응형 위치 구현 방식 변경
    ref 사용하는 방식에서 state 방식으로 변경

--- front/src/components/Feed/FeedFAB.tsx
+++ front/src/components/Feed/FeedFAB.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { ReactComponent as ZoomBtnSvg } from '../../assets/icons/zoom_out.svg';
 import { ReactComponent as WriteBtnSvg } from '../../assets/icons/add_circle.svg';
+import Modal from '../Modal/Modal';
+import useModal from '../Modal/useModal';
 import { flexBox } from '../../lib/styles/mixin';
 
-const FloatingDiv = styled.div`
+const FloatingDiv = styled.div<{ pos: number }>`
   ${flexBox(null, 'center', 'column')};
   position: fixed;
-  left: 0;
+  left: ${(props) => props.pos}px;
   bottom: 0;
   width: 60px;
   font-size: 12px;
@@ -19,16 +21,16 @@ const FloatingDiv = styled.div`
 `;
 
 const FeedFAB = () => {
-  const floatingEl = useRef<HTMLDivElement>(null);
+  const { isShowing, toggle } = useModal();
   const FEED_SECTION_WIDTH: number = 500;
   const FAB_OFFSET: number = 10;
+  const getFloatingPos = () => (window.innerWidth + FEED_SECTION_WIDTH) / 2 + FAB_OFFSET;
+  const [floatingPos, setFloatingPos] = useState(getFloatingPos());
   const changeResponsivePosition = () => {
-    const newPos: number = (window.innerWidth + FEED_SECTION_WIDTH) / 2;
-    if (floatingEl.current !== null) floatingEl.current.style.left = newPos + FAB_OFFSET + 'px';
+    setFloatingPos(getFloatingPos());
   };
 
   useEffect(() => {
-    changeResponsivePosition();
     window.addEventListener('resize', changeResponsivePosition);
     return () => {
       window.removeEventListener('resize', changeResponsivePosition);
@@ -36,11 +38,14 @@ const FeedFAB = () => {
   }, []);
 
   return (
-    <FloatingDiv ref={floatingEl}>
+    <FloatingDiv pos={floatingPos}>
       <div>탐험하기</div>
       <ZoomBtnSvg />
       <div>글쓰기</div>
-      <WriteBtnSvg />
+      <WriteBtnSvg onClick={toggle} />
+      <Modal isShowing={isShowing} hide={toggle}>
+        <div>adfafd</div>
+      </Modal>
     </FloatingDiv>
   );
 };
