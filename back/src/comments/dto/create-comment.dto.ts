350017815c35ef12f9acbaba004a71d6ecd0b68d
lee-3-8 <leesinji8@gmail.com>
2021-11-10 04:10:08 +0900

    feat: create comment dto 추가

--- back/src/comments/dto/create-comment.dto.ts
+++ back/src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,8 @@
-export class CreateCommentDto {}
+import { IsNotEmpty } from "class-validator";
+export class CreateCommentDto {
+  @IsNotEmpty()
+  post_id: number;
+
+  @IsNotEmpty()
+  content: string;
+}
