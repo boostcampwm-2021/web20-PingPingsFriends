998e62d7f53061faecea66bfba8fb7cf25cf25b6
ludacirs <ludacirs@naver.com>
2021-11-11 01:10:52 +0900

    fix: [#63] 피드 이미지 레이지 로딩
    IntersectionObserver를 이용해서 레이지 로딩

--- front/src/components/_common/Carousel/Slide.tsx
+++ front/src/components/_common/Carousel/Slide.tsx
@@ -1,30 +1,56 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { rectType } from '../../../lib/hooks/useClientRect';
+import { useGetDiv } from '../../../lib/hooks/useGetDiv';
 
 const SlideImg = styled.img<Pick<SlideProps, 'rect'>>`
   width: ${({ rect }) => rect.width}px;
-  height: ${({ rect }) => rect.height}px;
+  height: 100%;
   object-fit: cover;
 `;
 const Video = styled.video<Pick<SlideProps, 'rect'>>`
   width: ${({ rect }) => rect.width}px;
-  height: ${({ rect }) => rect.height}px;
+  height: 100%;
   object-fit: cover;
 `;
 
 interface SlideProps {
   src: string;
   rect: rectType;
+  scrollRef: React.MutableRefObject<HTMLDivElement>;
 }
 
-const Slide = ({ src, rect }: SlideProps) => {
-  return src.includes('mp4') ? (
-    <Video rect={rect} controls autoPlay>
-      <source src={src} type={'video/mp4'} />
-    </Video>
-  ) : (
-    <SlideImg src={src} rect={rect} alt="피드 이미지" />
+const Slide = ({ src, rect, scrollRef }: SlideProps) => {
+  const [feedContent, ref] = useGetDiv();
+
+  useEffect(() => {
+    if ('id' in feedContent) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            const target = entries[0].target.firstChild as HTMLImageElement;
+            target.src = target.dataset.lazy as string;
+          }
+        },
+        {
+          root: scrollRef.current,
+          rootMargin: `500px 0px`,
+        }
+      );
+      observer.observe(feedContent);
+    }
+  }, [feedContent]);
+
+  return (
+    <div ref={ref}>
+      {src.includes('mp4') ? (
+        <Video rect={rect} controls autoPlay>
+          <source src={src} type={'video/mp4'} />
+        </Video>
+      ) : (
+        <SlideImg src={`default_avatar.png`} data-lazy={src} rect={rect} alt="피드 이미지" />
+      )}
+    </div>
   );
 };
 
