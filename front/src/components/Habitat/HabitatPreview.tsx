6e62f80b0457ef3092fabd05529e669bc2d9345d
kanghohyeong <hobro.kang@gmail.com>
2021-11-10 23:06:57 +0900

    feat: [#48 #45] 사이드 서식지 컴포넌트 구현
    사이드 서식지 크기 및 내부 요소들의 크기를 반응형으로 구현
    최초 로딩 시 또는 fetch error 상황에서 loading UI 구현
    서식지 변경 시 색 변경 transition 적용

--- front/src/components/Habitat/HabitatPreview.tsx
+++ front/src/components/Habitat/HabitatPreview.tsx
@@ -1,49 +1,109 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import Avatar from '../_common/Avatar/Avatar';
 import { flexBox } from '../../lib/styles/mixin';
 import MagicNumber from '../../lib/styles/magic';
+import { Palette } from '../../lib/styles/Palette';
+import { compareTime } from '../../lib/utils/time';
 
-const HabitatPreviewBlock = styled.div<HabitatPreviewProps>`
+const HabitatPreviewBlock = styled.div<Pick<HabitatPreviewProps, 'side'> & { radius: number }>`
   ${flexBox('center', null, 'column')}
-
   position: absolute;
   border-radius: 50%;
-  top: 0;
   transform: translateY(50vh) ${(props) => `translateY(-${props.radius - parseInt(MagicNumber.HEADER_HEIGHT) / 2}px)`};
-  background: ${({ color }) => color};
+  background: ${({ color }) => color ?? Palette.GRAY};
+  cursor: pointer;
+  transition: background-color 1s ease-out 0s;
+  font-size: min(1.5vw, 18px);
 
   ${({ side, radius }) => {
     const commonTemplate = `
       width: ${radius * 2}px;
       height: ${radius * 2}px;
-      padding: ${radius / 3}px;
-      ${radius < 100 ? 'display:none' : ''}
     `;
     const leftTemplate = `
       left: -${radius}px;
-      align-items: flex-end;
+      align-items: flex-start;
+      padding-left: ${radius}px;
+      text-align: left;
     `;
     const rightTemplate = `
       right: -${radius}px;
-      align-items: flex-start;
+      align-items: flex-end;
+      padding-right: ${radius}px;
+      text-align: right;
     `;
     return side === 'right' ? rightTemplate + commonTemplate : leftTemplate + commonTemplate;
   }};
 `;
 
+const TitleDiv = styled.div<{ radius: number }>`
+  transform: translateY(${(props) => props.radius * 0.2}px);
+  white-space: nowrap;
+  .habitat_name {
+    font-weight: bold;
+    font-size: 2em;
+  }
+  .habitat_king {
+    font-size: 1em;
+  }
+`;
+
+const ShrinkTitleDiv = styled.div`
+  font-size: 2em;
+  font-weight: bold;
+  word-break: keep-all;
+`;
+
+const DetailDiv = styled.div<{ radius: number }>`
+  text-align: center;
+  font-size: 1.3em;
+  width: ${(props) => props.radius}px;
+  p {
+    margin-bottom: 0.5em;
+  }
+`;
+
+const AvatarDiv = styled.div<Pick<HabitatPreviewProps, 'side'> & { radius: number }>`
+  ${flexBox('space-evenly', 'center')};
+  width: ${(props) => props.radius}px;
+  margin-bottom: 1.5em;
+  ${(props) => {
+    if (props.side === 'left')
+      return css`
+        padding-right: 1em;
+      `;
+    else
+      return css`
+        padding-left: 1em;
+      `;
+  }}
+`;
+
 interface HabitatPreviewProps {
   side: string;
-  color: string;
-  radius: number;
+  habitat: number;
+  onClick: () => void;
 }
 
-const HabitatPreview = ({ side, color }: Omit<HabitatPreviewProps, 'radius'>) => {
+interface HabitatInfo {
+  recentUser: string[];
+  totalUser: number;
+  totalPost: number;
+  recentUpload: string;
+  king: string;
+  name: string;
+  color: string;
+}
+
+const HabitatPreview = ({ side, habitat, onClick }: HabitatPreviewProps) => {
   const HABITAT_OFFSET = 50;
   const getResponsiveRadius = () => Math.min((window.innerWidth - parseInt(MagicNumber.FEED_SECTION_WIDTH) - HABITAT_OFFSET) / 2, parseInt(MagicNumber.MAX_PREVIEW_RADIUS));
   const [radius, setRadius] = useState(getResponsiveRadius());
   const changeResponsiveRadius = () => {
     setRadius(getResponsiveRadius());
   };
+  const [habitatInfo, setHabitatInfo] = useState<HabitatInfo | undefined>(undefined);
 
   useEffect(() => {
     window.addEventListener('resize', changeResponsiveRadius);
@@ -52,20 +112,50 @@ const HabitatPreview = ({ side, color }: Omit<HabitatPreviewProps, 'radius'>) =>
     };
   }, []);
 
-  return (
-    <HabitatPreviewBlock side={side} color={color} radius={radius}>
-      {radius > 150 ? (
-        <>
-          <div>avatars</div>
-          <div>마리의 동물들</div>
-          <div>의 게시글</div>
-          <div>최근 활동: </div>
-          <div>우두머리</div>
-        </>
-      ) : null}
+  useEffect(() => {
+    if (habitat === undefined) return;
+    fetch(`/habitat/${habitat}.json`, { headers: { Accept: 'application/json' } })
+      .then((res) => res.json())
+      .then((json) => setHabitatInfo(json))
+      .catch((err) => {
+        console.log(err);
+        setHabitatInfo(undefined);
+      });
+  }, [habitat]);
 
-      <div>장소</div>
-    </HabitatPreviewBlock>
+  return (
+    <>
+      {radius > 100 && (
+        <HabitatPreviewBlock side={side} color={habitatInfo?.color} radius={radius} onClick={onClick}>
+          {habitatInfo !== undefined ? (
+            <>
+              {radius > 150 ? (
+                <>
+                  <AvatarDiv radius={radius} side={side}>
+                    {habitatInfo.recentUser.map((imageURL, idx) => (
+                      <Avatar imgSrc={imageURL} key={idx} size={'3em'} />
+                    ))}
+                  </AvatarDiv>
+                  <DetailDiv radius={radius}>
+                    <p>{habitatInfo.totalUser} 마리의 동물들</p>
+                    <p>{habitatInfo.totalPost}개의 게시글</p>
+                    <p>최근 활동 {compareTime(new Date(), new Date(habitatInfo.recentUpload))}</p>
+                  </DetailDiv>
+                  <TitleDiv radius={radius}>
+                    <p className="habitat_king">우두머리: {habitatInfo.king}</p>
+                    <p className="habitat_name">{habitatInfo.name}</p>
+                  </TitleDiv>
+                </>
+              ) : (
+                <ShrinkTitleDiv>{habitatInfo.name}</ShrinkTitleDiv>
+              )}
+            </>
+          ) : (
+            'Loading...'
+          )}
+        </HabitatPreviewBlock>
+      )}
+    </>
   );
 };
 
