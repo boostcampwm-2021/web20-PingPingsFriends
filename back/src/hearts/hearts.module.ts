09953fd3d906845d517de8d5b3d045be0413551f
9b2n <rbqls1057@gmail.com>
2021-11-11 09:25:55 +0900

    fix : [#97] 좋아요 repository 종속성 주입

--- back/src/hearts/hearts.module.ts
+++ back/src/hearts/hearts.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HeartsService } from './hearts.service';
 import { HeartsController } from './hearts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Heart } from './entities/heart.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Heart])],
   controllers: [HeartsController],
-  providers: [HeartsService]
+  providers: [HeartsService],
 })
 export class HeartsModule {}
