caece7f93612c67a29c94fc4b38c1f4cce3068ec
9b2n <rbqls1057@gmail.com>
2021-11-11 20:57:34 +0900

    refactor: app단 controller, service 삭제

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { S3Module } from './s3/s3.module';
 import { ContentsModule } from './contents/contents.module';
