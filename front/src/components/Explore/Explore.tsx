d6909de18903a4e2e5b451b13cf8874f35a2fb9f
kanghohyeong <hobro.kang@gmail.com>
2021-11-17 02:43:53 +0900

    fix: [#121] 화면 전환 애니메이션 씹힘 버그 수정
    transition 으로 화면 확대 애니메이션 구현 시 종종 씹히는 현상 발견
    keyframes 애니메이션으로 구현 방식 변경

--- front/src/components/Explore/Explore.tsx
+++ front/src/components/Explore/Explore.tsx
@@ -4,19 +4,26 @@ import styled from 'styled-components';
 import { Palette } from '@lib/styles/Palette';
 
 const ExploreDiv = styled.div<{ color: string | undefined }>`
-  width: 300px;
+  width: 100%;
   margin: auto;
   background-color: ${(props) => (props.color !== undefined ? props.color : Palette.PINK)};
   z-index: 1;
-  transition: width 1s ease;
   height: 100%;
+  animation-duration: 1s;
+  animation-name: zoomout;
+  @keyframes zoomout {
+    from {
+      width: 300px;
+    }
+    to {
+      width: 100%;
+    }
+  }
 `;
 
 const Explore = ({ habitatInfo }: { habitatInfo: HabitatInfo | undefined }) => {
   const divRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (divRef.current) divRef.current.style.width = '100%';
-  }, []);
+
   return (
     <ExploreDiv ref={divRef} color={habitatInfo?.color}>
       adafds
