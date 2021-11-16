861b99942333f48656314e1492ef9ac799dbea70
lee-3-8 <leesinji8@gmail.com>
2021-11-17 04:03:43 +0900

    chore: comment api swagger 추가

--- back/src/comments/comments.controller.ts
+++ back/src/comments/comments.controller.ts
@@ -1,23 +1,55 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Comment } from './entities/comment.entity';
 
 @Controller('comments')
+@ApiTags('댓글 API')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
   @Post() //댓글 추가
+  @ApiOperation({
+    summary: '댓글 추가 API',
+    description: '게시물에 댓글을 추가한다.',
+  })
+  @ApiCreatedResponse({ description: '댓글 생성', type: Comment })
   create(@Body() createCommentDto: CreateCommentDto) {
     return this.commentsService.createComment(createCommentDto);
   }
 
-  @Patch(':id')//댓글 수정
-  update(@Param('id', ParseIntPipe) id: number, @Body() updateCommentDto: UpdateCommentDto) {
+  @Patch(':id') //댓글 수정
+  @ApiOperation({
+    summary: '댓글 수정 API',
+    description: '게시물 댓글을 수정한다.',
+  })
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateCommentDto: UpdateCommentDto
+  ) {
     return this.commentsService.updateComment(id, updateCommentDto);
   }
 
-  @Delete(':id')//댓글 삭제
+  @Delete(':id') //댓글 삭제
+  @ApiOperation({
+    summary: '댓글 삭제 API',
+    description: '게시물 댓글을 삭제한다.',
+  })
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.commentsService.removeComment(id);
   }
