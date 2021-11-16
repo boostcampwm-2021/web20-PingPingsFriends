12dc739146973b787742483857018b5d778f0809
lee-3-8 <leesinji8@gmail.com>
2021-11-17 07:09:19 +0900

    faet: user repository 추가

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,19 +1,9 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+
 import {
   ApiTags,
   ApiOperation,
-  ApiResponse,
   ApiCreatedResponse,
 } from '@nestjs/swagger';
 import { userResponseDto } from './dto/userResponseDto';
@@ -28,7 +18,10 @@ export class UsersController {
     summary: '유저 조회',
     description: '모든 유저를 조회하는 api입니다.',
   })
-  @ApiCreatedResponse({ description: '성공', type: [userResponseDto] })
+  @ApiCreatedResponse({
+    description: '성공',
+    type: [userResponseDto],
+  })
   findAll() {
     return this.usersService.findAll();
   }
