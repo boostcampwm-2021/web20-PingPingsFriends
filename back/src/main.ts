76359d358c5f94538a6378969fee8912081ab7e4
kanghohyeong <hobro.kang@gmail.com>
2021-11-18 21:02:05 +0900

    feat: cors 설정
    Co-authored-by: lee-3-8 <Lee-3-8@users.noreply.github.com>

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api', { exclude: ['docs'] });
   const config = new DocumentBuilder()
     .setTitle(`PingPing's Friends`)
