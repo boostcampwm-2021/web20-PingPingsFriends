56c56c98f72944bb29664061f3fbd85346d5881c
kanghohyeong <hobro.kang@gmail.com>
2021-11-18 21:06:49 +0900

    feat: 댓글 API 추가
    Co-authored-by: lee-3-8 <Lee-3-8@users.noreply.github.com>

--- back/src/comments/comments.controller.ts
+++ back/src/comments/comments.controller.ts
@@ -7,14 +7,12 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  Query,
 } from '@nestjs/common';
-import {
-  ApiCreatedResponse,
-  ApiOperation,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { CursorPaginationDto } from './dto/cursor-pagination.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 import { Comment } from './entities/comment.entity';
 
@@ -38,10 +36,7 @@ export class CommentsController {
     summary: '댓글 수정 API',
     description: '게시물 댓글을 수정한다.',
   })
-  update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() updateCommentDto: UpdateCommentDto
-  ) {
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateCommentDto: UpdateCommentDto) {
     return this.commentsService.updateComment(id, updateCommentDto);
   }
 
@@ -52,5 +47,23 @@ export class CommentsController {
   })
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.commentsService.removeComment(id);
+  }
+
+  @Get('cursor') //댓글 커서 페이지네이션
+  @ApiOperation({
+    summary: '댓글 리스트 반환 API',
+    description: '게시물(postId)의 특정 댓글(lastId)부터 특정 개수(limit)를 반환하는 API',
+  })
+  getCommentList(@Query() query: CursorPaginationDto) {
+    return this.commentsService.getCommentList(query);
+  }
+
+  @Get('all:postId') //댓글 전체반환
+  @ApiOperation({
+    summary: '댓글 리스트 반환 API',
+    description: '게시물(postId)의 특정 댓글(lastId)부터 특정 개수(limit)를 반환하는 API',
+  })
+  getCommentList(@Param('postId', ParseIntPipe) postId: number) {
+    return this.commentsService.getCommentAll(postId);
   }
 }
--- back/src/comments/comments.service.ts
+++ back/src/comments/comments.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CommentRepository } from './comment.repository';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { CursorPaginationDto } from './dto/cursor-pagination.dto';
 
 @Injectable()
 export class CommentsService {
   constructor(
     @InjectRepository(CommentRepository)
-    private commentRepository: CommentRepository){}
+    private commentRepository: CommentRepository
+  ) {}
 
   createComment(createCommentDto: CreateCommentDto) {
     return this.commentRepository.createComment(createCommentDto);
@@ -20,6 +22,13 @@ export class CommentsService {
 
   removeComment(id: number) {
     return this.commentRepository.removeComment(id);
+  }
 
+  getCommentList({ lastId, postId, limit }: CursorPaginationDto) {
+    return this.commentRepository.selectCommentListByCursor(lastId);
+  }
+
+  getCommentAll(postId: number){
+    return this.commentRepository.findAll(postId);
   }
 }
