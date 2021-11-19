88d6daf439576330f221eea85d97f3fc18a3e06a
ludacirs <ludacirs@naver.com>
2021-11-19 23:50:06 +0900

    feat: [#164] sql Date 형식 포맷 함수 구현
    문자열로 되어있는 sql Date를 파싱하고 가장 큰 단위 날짜로 포매팅해주는 함수 구현
    사실 이전에 자바스크립트 프로젝트에 쓰던걸 그대로 가져와서 타입은 다 any로 퉁침.. 고쳐야함

--- front/src/lib/utils/time.ts
+++ front/src/lib/utils/time.ts
@@ -1,6 +1,58 @@
+import TIME from '@lib/constants/timeUnit';
+
 export const compareTime = (from: Date, to: Date) => {
   const milliDiff = from.getTime() - to.getTime();
   const hourDiff = Math.floor(milliDiff / (60 * 60 * 1000));
   if (hourDiff < 24) return hourDiff.toString() + '시간 전';
   return Math.floor(hourDiff / 24).toString() + '일 전';
 };
+
+export const formatDate = (sqlDate: string) => {
+  const [date, time] = sqlDate.split('T');
+  const [YYYY, MM, DD] = date.split('-');
+  const [hh, mm] = time.split(':');
+
+  const publishedStampTime = new Date(+YYYY, +MM - 1, +DD, +hh, +mm).getTime();
+  const currentStampTime = new Date().getTime();
+
+  const second = Math.floor((currentStampTime - publishedStampTime) / TIME.MS);
+
+  //todo: 이전에 sqlDate를 변환해주는 함수 구현한 걸 그대로 가져와서 타입 정의가 필요함
+  // 가장 큰 단위의 날짜를 반환해줌 ex)1년 전, 5개월 전, 3주 전, 30초 전...
+  const pastDate = pipe(divisionYear, divisionMonth, divisionWeek, divisionDay, divisionHour, divisionMin)(second);
+
+  return `${pastDate[1]} ${pastDate[0]}`;
+};
+
+const divisionYear = (second: any): any => {
+  const quotient = second / TIME.YEAR;
+  return quotient >= 1 ? ['년', quotient.toFixed(0)] : second;
+};
+const divisionMonth = (second: any): any => {
+  const quotient = second / TIME.MONTH;
+  return quotient >= 1 ? ['개월', quotient.toFixed(0)] : second;
+};
+const divisionWeek = (second: any): any => {
+  const quotient = second / TIME.WEEK;
+  return quotient >= 1 ? ['주', quotient.toFixed(0)] : second;
+};
+const divisionDay = (second: any): any => {
+  const quotient = second / TIME.DAY;
+  return quotient >= 1 ? ['일', quotient.toFixed(0)] : second;
+};
+const divisionHour = (second: any): any => {
+  const quotient = second / 3600;
+  return quotient >= 1 ? ['시간', quotient.toFixed(0)] : second;
+};
+const divisionMin = (second: any): any => {
+  const quotient = second / 60;
+  return quotient >= 1 ? ['분', quotient.toFixed(0)] : second;
+};
+
+const pipe =
+  (...functions: any[]) =>
+  (init: any) => {
+    return functions.reduce((value, func) => {
+      return func(value);
+    }, init);
+  };
