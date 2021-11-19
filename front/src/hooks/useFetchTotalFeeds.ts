3a8b70d20c9d638d6238b871ab90b63911d265b7
ludacirs <ludacirs@naver.com>
2021-11-19 23:47:05 +0900

    feat: [#156] unsplash 걷어내고 백엔드API 적용
    unsplash API로 데이터를 받아오는 부분 걷어냄
    백엔드 API 적용
    image urls을 파싱해서 배열 상태로 넣을 contents_url_array 프로퍼티 추가
    useFetchTotalFeeds의 fetchData 로직은 이것보다 나은게 생각나지 않음...

---
+++ front/src/hooks/useFetchTotalFeeds.ts
@@ -0,0 +1,44 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Posts, PostsResponse } from '@src/types/Post';
+import Config from '@lib/constants/config';
+
+const useFetchTotalFeeds = (curHabitatId: number): [Posts, Dispatch<SetStateAction<number | null>>] => {
+  const [feeds, setFeeds] = useState<Posts>([] as Posts);
+  const [lastFeedId, setLastFeedId] = useState<number | null>(null);
+  const [totalFeed, setTotalFeed] = useState<Posts>([] as Posts);
+
+  useEffect(() => {
+    setTotalFeed([]);
+    setLastFeedId(null);
+  }, [curHabitatId]);
+
+  useEffect(() => {
+    fetchData();
+
+    async function fetchData() {
+      try {
+        const response: Response = await fetch(`${Config.BACK_HOST}/api/posts/habitats/${curHabitatId}${lastFeedId ? `?lastPostId=${lastFeedId}` : ''}`);
+        const data: PostsResponse = await response.json();
+
+        if (!data.posts.length) {
+          setFeeds([]);
+          return;
+        }
+        // post에 이미지가 null인 경우가 존재해서 임시로 아래의 로직 사용 중
+        // const postsData = data.posts.map((post) => ({ ...post, contents_url_array: post.post_contents_urls.split(',') }));
+        const postsData = data.posts.map((post) => ({ ...post, contents_url_array: post.post_contents_urls ? post.post_contents_urls.split(',') : ['/default_avatar.png'] }));
+        setFeeds(postsData);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }, [lastFeedId, curHabitatId]);
+
+  useEffect(() => {
+    setTotalFeed([...totalFeed, ...feeds]);
+  }, [feeds]);
+
+  return [totalFeed, setLastFeedId];
+};
+
+export default useFetchTotalFeeds;
