8bb84b4b4e507f943ed426d5cd6dda7956206c61
lee-3-8 <leesinji8@gmail.com>
2021-11-21 15:05:34 +0900

    feat: follow api 초기화

---
+++ back/src/follow/follow.repository.ts
@@ -0,0 +1,5 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Follow } from './entities/follow.entity';
+
+@EntityRepository(Follow)
+export class FollowRepository extends Repository<Follow> {}
--- back/src/follow/follow.service.ts
+++ back/src/follow/follow.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateFollowDto } from './dto/create-follow.dto';
 import { UpdateFollowDto } from './dto/update-follow.dto';
+import { FollowRepository } from './follow.repository';
 
 @Injectable()
 export class FollowService {
+  constructor(
+    @InjectRepository(FollowRepository)
+    private followRepository: FollowRepository
+  ) {}
   create(createFollowDto: CreateFollowDto) {
     return 'This action adds a new follow';
   }
