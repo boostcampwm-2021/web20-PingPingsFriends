fde282c2874998eac7fe076af1a5914b80d579fb
lee-3-8 <leesinji8@gmail.com>
2021-11-21 15:55:21 +0900

    feat: swagger ui Auth 추가

--- back/src/comments/comments.controller.ts
+++ back/src/comments/comments.controller.ts
@@ -12,7 +12,7 @@ import {
   Req,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { CommentCursorPaginationDto } from './dto/comments-cursor-pagination.dto';
@@ -24,40 +24,40 @@ import { Comment } from './entities/comment.entity';
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
+  @ApiBearerAuth('access-token')
   @Post() //댓글 추가
+  @UseGuards(AuthGuard('jwt'))
   @ApiOperation({
     summary: '댓글 추가 API',
     description: '게시물에 댓글을 추가한다.',
   })
-  @UseGuards(AuthGuard('jwt'))
   @ApiCreatedResponse({ description: '댓글 생성', type: Comment })
   create(@Body() createCommentDto: CreateCommentDto, @Req() req) {
-    console.log(req);
     return this.commentsService.createComment(createCommentDto, req.user.userId);
   }
 
-  @Patch(':id') //댓글 수정
+  @Patch(':commentId') //댓글 수정
   @ApiOperation({
     summary: '댓글 수정 API',
     description: '게시물 댓글을 수정한다.',
   })
   @UseGuards(AuthGuard('jwt'))
   update(
-    @Param('id', ParseIntPipe) id: number,
+    @Param('commentId', ParseIntPipe) commentId: number,
     @Body() updateCommentDto: UpdateCommentDto,
     @Req() req
   ) {
-    return this.commentsService.updateComment(id, updateCommentDto);
+    return this.commentsService.updateComment(commentId, updateCommentDto);
   }
 
-  @Delete(':id') //댓글 삭제
+  @Delete(':commentId') //댓글 삭제
   @ApiOperation({
     summary: '댓글 삭제 API',
     description: '게시물 댓글을 삭제한다.',
   })
   @UseGuards(AuthGuard('jwt'))
-  remove(@Param('id', ParseIntPipe) id: number, @Req() req) {
-    return this.commentsService.removeComment(id);
+  remove(@Param('commentId', ParseIntPipe) commentId: number, @Req() req) {
+    return this.commentsService.removeComment(commentId);
   }
 
   @Get('cursor') //댓글 커서 페이지네이션
