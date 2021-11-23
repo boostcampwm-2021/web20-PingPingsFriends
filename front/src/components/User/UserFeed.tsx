473382615a00b2522e25d5d979d0d8ca84368d08
kanghohyeong <hobro.kang@gmail.com>
2021-11-23 19:04:49 +0900

    feat: [#183] 유저 페이지 구현

---
+++ front/src/components/User/UserFeed.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { UserFeeds } from '@src/types/User';
+import styled from 'styled-components';
+import { prettyScroll } from '@src/lib/styles/mixin';
+import MagicNumber from '@src/lib/styles/magic';
+
+interface UserFeedProps {
+  userId: number | undefined;
+}
+
+const UserFeed = ({ userId }: UserFeedProps) => {
+  const [feeds, setFeeds] = useState<UserFeeds>([]);
+  useEffect(() => {
+    const fetchUserFeed = async () => {
+      const res: Response = await fetch(`/api/posts/users/${userId}`);
+      if (res.ok) {
+        const data: UserFeeds = await res.json();
+        setFeeds(data);
+      }
+    };
+    fetchUserFeed();
+  }, [userId]);
+  return (
+    <FeedGridDiv>
+      {feeds.map(({ postId, url }) => (
+        <FeedCell>
+          <img src={url} key={postId} alt={'feed img'} />
+        </FeedCell>
+      ))}
+    </FeedGridDiv>
+  );
+};
+
+export default UserFeed;
+
+const FeedGridDiv = styled.div`
+  ${prettyScroll()};
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  justify-content: space-between;
+  width: 100%;
+  overflow-y: scroll;
+  height: calc(100% - 180px);
+`;
+
+const FeedCell = styled.div`
+  width: 150px;
+  height: 150px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
