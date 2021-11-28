c81de5e3d86e47205795bbe7db70c8142d0dd299
ludacirs <ludacirs@naver.com>
2021-11-28 16:27:17 +0900

    fix: [#271] 프로필 이미지 편집 구현
    프로필 이미지 편집 구현
    파일 업로드 후 api 응답이 올 때 까지 로딩창 구현

--- front/src/components/User/UserAbout.tsx
+++ front/src/components/User/UserAbout.tsx
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { User } from '@src/types/User';
 import styled from 'styled-components';
 import { flexBox } from '@src/lib/styles/mixin';
 import Avatar from '@common/Avatar/Avatar';
 import { Palette } from '@src/lib/styles/Palette';
+import { ReactComponent as PhotoCameraSvg } from '@assets/icons/photo_camera.svg';
+import { getAuthOption } from '@lib/utils/fetch';
+import { useUserState } from '@src/contexts/UserContext';
+import Modal from '@common/Modal/Modal';
+import AlertDiv from '@common/Alert/AlertDiv';
 
 interface UserAboutProps {
   userInfo: User | null;
 }
 
 const UserAbout = ({ userInfo }: UserAboutProps) => {
+  const { data: userData } = useUserState();
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLoading(true);
+    const target = e.target as HTMLInputElement;
+    const file = target.files![0];
+
+    const data = new FormData();
+    data.append('upload', file);
+    try {
+      const response = await fetch('/api/users/contents', getAuthOption('PATCH', userData!.accessToken, data));
+      await response.json();
+      setLoading(false);
+      window.location.reload();
+    } catch (e) {
+      setLoading(false);
+    }
+  };
+
   return (
     <UserAboutDiv>
-      <AvatarDiv>
-        <Avatar imgSrc={userInfo?.content?.url} size={'100%'} />
-      </AvatarDiv>
+      {userData?.userId === userInfo?.id ? (
+        <Form>
+          <FileInsertLabel htmlFor="profile">
+            <AvatarDiv>
+              <Avatar imgSrc={userInfo?.content?.url} size={'100%'} preventLink />
+            </AvatarDiv>
+            <SvgContainer>
+              <PhotoCameraSvg />
+            </SvgContainer>
+          </FileInsertLabel>
+          <FileInput id="profile" type="file" accept="image/*" name="contents" form="write" onChange={handleChange} />
+        </Form>
+      ) : (
+        <AvatarDiv>
+          <Avatar imgSrc={userInfo?.content?.url} size={'100%'} preventLink />
+        </AvatarDiv>
+      )}
+
       <TextDiv>
         {userInfo ? (
           <>
@@ -26,6 +66,11 @@ const UserAbout = ({ userInfo }: UserAboutProps) => {
           '존재하지 않는 사용자입니다'
         )}
       </TextDiv>
+      {loading && (
+        <Modal isShowing={true}>
+          <AlertDiv> 파일 업로드 중입니다.</AlertDiv>
+        </Modal>
+      )}
     </UserAboutDiv>
   );
 };
@@ -60,4 +105,33 @@ const TextDiv = styled.div`
   .nickname {
     font-size: 50px;
   }
+`;
+
+const FileInsertLabel = styled.label`
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  text-align: center;
+  position: relative;
+  background: rgba(255, 255, 255, 0);
+`;
+
+const SvgContainer = styled.div`
+  ${flexBox('center', 'center')};
+  position: absolute;
+  z-index: 1;
+  width: 30px;
+  height: 30px;
+  background: white;
+  border-radius: 50%;
+  bottom: 0;
+  right: 0;
+`;
+
+const FileInput = styled.input`
+  display: none;
+`;
+
+const Form = styled.form`
+  ${flexBox('center', 'center', 'column')};
 `;
--- front/src/components/_common/Alert/AlertDiv.tsx
+++ front/src/components/_common/Alert/AlertDiv.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import { boxShadow } from '@lib/styles/mixin';
+import { boxShadow, flexBox } from '@lib/styles/mixin';
 import { Palette } from '@lib/styles/Palette';
 
 const AlertDivBlock = styled.div`
   ${boxShadow('20px')};
+  ${flexBox('center', 'center', 'column')};
   background-color: ${Palette.WHITE};
   width: 300px;
   height: 200px;
   text-align: center;
-  line-height: 200px;
+  line-height: 80px;
 `;
 
 const AlertDiv = ({ children }: { children: React.ReactNode }) => {
