28f617538cb5bc1abf2c16bfa89d40c7f1992a06
ludacirs <ludacirs@naver.com>
2021-11-29 10:43:56 +0900

    fix: [] 좋아요 요청 수정

--- front/src/components/HeartButton/useLike.ts
+++ front/src/components/HeartButton/useLike.ts
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { getAuthOption } from '@lib/utils/fetch';
+import { useUserState } from '@src/contexts/UserContext';
 
 export interface LikeProps {
   like: boolean;
@@ -10,19 +12,16 @@ export type UseLikeType = (isHeartString: 0 | 1, feedId: number) => [like: boole
 export const useLike: UseLikeType = (isHeartString, feedId) => {
   const isHeart = isHeartString !== 0;
   const [like, setLike] = useState(isHeart);
+  const userState = useUserState();
 
   const toggleLike = () => {
     if (like) {
       setLike(false);
-      fetch(`/api/hearts/${feedId}`, {
-        method: 'DELETE',
-      });
+      fetch(`/api/hearts/${feedId}`, getAuthOption('DELETE', userState.data?.accessToken));
       return;
     }
     setLike(true);
-    fetch(`/api/hearts/${feedId}`, {
-      method: 'POST',
-    });
+    fetch(`/api/hearts/${feedId}`, getAuthOption('POST', userState.data?.accessToken));
   };
 
   return [like, toggleLike];
