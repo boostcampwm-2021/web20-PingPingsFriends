c8ec29324fa5a8b61e7408a0836cb4183554a11f
lee-3-8 <leesinji8@gmail.com>
2021-11-30 13:14:42 +0900

    fix: validation hotfix

--- back/common/dto/cursor-pagination.dto.ts
+++ back/common/dto/cursor-pagination.dto.ts
@@ -1,15 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsNotEmpty, IsOptional, Max, Min } from 'class-validator';
+import { IsInt, IsNotEmpty, IsOptional, IsPositive } from 'class-validator';
 export class CursorPaginationDto {
   @IsOptional()
-  @IsInt()
-  @Min(1)
   @ApiProperty({ required: false })
   lastId?: number;
 
   @ApiProperty()
-  @IsInt()
-  @Min(0)
   @IsNotEmpty()
   limit: number;
 }
--- back/common/dto/pagination-query.dto.ts
+++ back/common/dto/pagination-query.dto.ts
@@ -1,18 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsNotEmpty, Max, Min } from 'class-validator';
+import { IsInt, IsNotEmpty, IsPositive, Min } from 'class-validator';
 
 export class PaginationQueryDto {
   @IsNotEmpty()
-  @IsInt()
-  @Min(0)
-  @Max(100)
   @ApiProperty()
   skip: number;
 
   @IsNotEmpty()
-  @IsInt()
-  @Min(0)
-  @Max(100)
   @ApiProperty()
   take: number;
 }
