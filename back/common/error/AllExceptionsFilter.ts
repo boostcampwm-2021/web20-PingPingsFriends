83cbc16908bd85130c35f770d1d81d070571df38
lee-3-8 <leesinji8@gmail.com>
2021-11-30 12:41:53 +0900

    fix: [#171] exceptionfilter 수정

--- back/common/error/AllExceptionsFilter.ts
+++ back/common/error/AllExceptionsFilter.ts
@@ -1,16 +1,41 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { QueryFailedError } from 'typeorm';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
-  catch(exception: unknown, host: ArgumentsHost) {
+  catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
     const request = ctx.getRequest();
-    let message = (exception as any).response.message;
-    console.log(exception);
-    const status =
-      exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = '';
+    let status = 200;
+    process.env.NODE_ENV === 'dev' && console.log(exception);
+    switch (exception.constructor) {
+      case HttpException:
+      case BadRequestException:
+      case NotFoundException:
+        status = exception.getStatus();
+        message = (exception as any).response.message;
+        break;
+      case QueryFailedError:
+        message = 'DB 애러입니다.';
+        status = HttpStatus.SERVICE_UNAVAILABLE;
+        break;
 
+      default:
+        status = HttpStatus.INTERNAL_SERVER_ERROR;
+        message = 'internal server error';
+    }
     response.status(status).json({
       statusCode: status,
       timestamp: new Date().toISOString(),
